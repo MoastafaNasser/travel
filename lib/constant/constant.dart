6c7b854e847d90cc28a9e047b356ccceede578a8
MostafaNasser <https://github.com/MoastafaNasser>
2025-02-17 14:20:56 +0200

    change tap bar screen

--- lib/constant/constant.dart
+++ lib/constant/constant.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 const kPrimaryStyle = TextStyle(
   fontSize: 24,
   color: Colors.white,
-  fontFamily: "ElMessiri",
+  
 );
--- lib/screens/tab_Screen.dart
+++ lib/screens/tab_Screen.dart
@@ -1,36 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:travel/constant/constant.dart';
 import 'package:travel/screens/categories_screen.dart';
 import 'package:travel/screens/favorite_Screeen.dart';
 
-class TabScreen extends StatelessWidget {
+class TabScreen extends StatefulWidget {
   const TabScreen({super.key});
 
   @override
+  State<TabScreen> createState() => _TabScreenState();
+}
+
+class _TabScreenState extends State<TabScreen> {
+  void _selectScreen(int index) {
+    setState(() {
+      _selectScreenindex = index;
+    });
+  }
+
+  int _selectScreenindex = 0;
+
+  final List<Map<String, dynamic>> _screens = [
+    {
+      "Screen": CategoriesScreen(),
+      "title": "تصنيفات الرحلات ",
+    },
+    {
+      "Screen": FavoriteScreeen(),
+      "title": "الرحلات المفضله ",
+    },
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("دليل سياحي "),
-          bottom: TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(Icons.dashboard),
-                text: "التصنيفات ",
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: "المفضله  ",
-              ),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        title: Center(
+          child: Text(
+            _screens[_selectScreenindex]["title"],
+            style: kPrimaryStyle,
           ),
         ),
-        body: TabBarView(
-          children: [
-            CategoriesScreen(),
-            FavoriteScreeen(),
-          ],
-        ),
+      ),
+      body: _screens[_selectScreenindex]["Screen"],
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Theme.of(context).hintColor,
+        unselectedItemColor: Colors.white,
+        currentIndex: _selectScreenindex,
+        onTap: _selectScreen,
+        backgroundColor: Theme.of(context).primaryColor,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.dashboard), label: "التصنيفات "),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: "المفضله  "),
+        ],
       ),
     );
   }
